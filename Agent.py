807caa97564fa963396bdb6bcb74cb1638a438f9
Dan Alexandru <dan.alexandru@info.uaic.ro>
2017-01-10 23:58:19 +0200

    refactored neural network

--- Agent.py
+++ Agent.py
@@ -1,15 +1,16 @@
 from __future__ import print_function
 import random
 import datetime
+from memory import add, get_current_state, get_minibatch
 
 
 class Agent:
-    def __init__(self, environment, memory, q_network, random_starts=30, history_length=4,
-                 exploration_rate_start=1, exploration_rate_end=0.1, exploration_decay_steps=1000000.0,
-                 exploration_rate_test=0.05, train_frequency=4, train_repeat=1):
+    def __init__(self, environment, q_network,
+                 start_epoch=0, train_steps=250000, random_starts=30, history_length=4,
+                 exploration_rate_start=1, exploration_rate_end=0.1, exploration_decay_steps=1000000,
+                 train_frequency=4, train_repeat=1):
 
         self.env = environment
-        self.mem = memory
         self.net = q_network
 
         self.num_actions = self.env.num_actions()
@@ -20,13 +21,11 @@ class Agent:
         self.exploration_rate_start = exploration_rate_start
         self.exploration_rate_end = exploration_rate_end
         self.exploration_decay_steps = exploration_decay_steps
-        self.exploration_rate_test = exploration_rate_test
         self.train_frequency = train_frequency
         self.train_repeat = train_repeat
 
-        self.total_train_steps = 0
-
-        self.callback = None
+        self.train_steps = train_steps
+        self.total_train_steps = start_epoch * train_steps
 
         self.total_score = 0.0
         self.total_moves = 0
@@ -40,13 +39,14 @@ class Agent:
     def restart_random(self):
         self.env.restart()
         # perform random number of dummy actions to produce more stochastic games
-        for i in xrange(random.randint(self.history_length, self.random_starts) + 1):
+        for i in range(random.randint(self.history_length, self.random_starts) + 1):
             reward = self.env.act(0)
             screen = self.env.get_screen()
             terminal = self.env.is_terminal()
             assert not terminal, "terminal state occurred during random initialization"
             # add dummy states to buffer to guarantee history_length screens
-            self.mem.add(0, reward, screen, terminal)
+            #self.buf.add(screen)
+            add(0, reward, screen, terminal)
 
     def _exploration_rate(self):
         # calculate decaying exploration rate - very slowly slowly go from 0.1 to 1.0 over decay_steps steps
@@ -66,16 +66,14 @@ class Agent:
             action = random.randrange(self.num_actions)
         else:
             # choose action with highest Q-value
-            state = self.mem.getCurrentState()
+            state = get_current_state()
             # for convenience getCurrentState() returns minibatch
-            action = self.net.choose_action(state, 0)
+            action = self.net.choose_action(state)
             # print "Prediction chosen",action
-            moves_print = "choosing to predict stuff! Exploration rate:	" + str(
-                exploration_rate) + " action is: " + str(action)
-        # print moves_print
-        # moves_chosen = open("moves.txt","a")
-        # moves_chosen.write(moves_print+"\n")
-        # moves_chosen.close()
+            # moves_print = " Exploration rate:	" + str(exploration_rate) + " action is: " + str(action)
+            # moves_chosen = open("moves.txt", "a")
+            # moves_chosen.write(moves_print+"\n")
+            #  moves_chosen.close()
 
         reward = self.env.act(action)
         screen = self.env.get_screen()
@@ -83,7 +81,7 @@ class Agent:
 
         self.total_score += reward
 
-        self.mem.add(action, reward, screen, terminal)
+        add(action, reward, screen, terminal)
 
         # if game over, then restart it
         if terminal:
@@ -97,48 +95,33 @@ class Agent:
             self.total_score = 0
             self.total_moves = 0
             self.restart_random()
-
-        # call callback to record statistics
-        if self.callback:
-            self.callback.on_step(action, reward, terminal, screen, exploration_rate)
-
         return terminal
 
     def play_random(self, random_steps):
         # randomly play for given no. of steps
-        for i in xrange(random_steps):
+        for i in range(random_steps):
             # use exploration rate 1 = completely random
             self.step(1)
-            print("random step", i)
+            #print("random step", i)
 
-    def train(self, train_steps, epoch=0):
-        traincount = 0
-        assert self.mem.count >= self.history_length, "Not enough history in replay memory, increase random steps."
+    def train(self):
         # play given number of steps
-        for i in xrange(train_steps):
+        for i in range(self.train_steps):
             # perform a single game step
             self.step(self._exploration_rate())
             # train after every train_frequency (4)	steps
             if i % self.train_frequency == 0:
                 # train for train_repeat times
-                for j in xrange(self.train_repeat):
+                for j in range(self.train_repeat):
                     # sample minibatch
-                    minibatch = self.mem.getMinibatch()
-                    loss = self.net.train(minibatch[0], minibatch[1], minibatch[2], minibatch[3], minibatch[4])
-                    traincount += 1
-                    log = open("model_loss.txt", "a")
-                    log.write("Train Count: " + str(traincount) + "			Loss: " + str(loss) + "\n")
-                    log.close()
-            log = open("model_loss.txt", "a")
-            # log.write("---------------------------------------------------------------------------------\n")
-            log.write("TOTAL TRAIN STEP: " + str(self.total_train_steps) + "\n")
-            log.close()
+                    minibatch = get_minibatch()
+                    self.net.train(minibatch[0], minibatch[1], minibatch[2], minibatch[3], minibatch[4])
             self.total_train_steps += 1
 
     def play(self, num_games):
         self.restart_random()
        # self.env.render()
-        for i in xrange(num_games):
+        for i in range(num_games):
             # play until terminal state
-            while not self.step(self.exploration_rate_test, play=True):
+            while not self.step(0.05, play=True):
                 pass
